b038c47060b4c20c82b584ca738b8de7202e067d
jay-228 <jay.mistry2208@gmail.com>
2024-05-22 14:54:47 +0530

    neww

--- src/cpmonent/Poster.jsx
+++ src/cpmonent/Poster.jsx
@@ -1,58 +1,9 @@
-// import React from 'react'
-// import Button from 'react-bootstrap/Button';
-// import "bootstrap-icons/font/bootstrap-icons.css";
-// function Poster() {
-//   return (
-//     <div className='App'> 
-//         <div className='Container'>
-//             <div className="row">
-//                 <div className="col-6">
-//                     <div className="context">
-//                     <h3>Hi, I'm JAY MISTRY</h3>
-//                     <h1>WEB DEVLOPER</h1>
-//                     <p>I'm a professional web Developer. our main goal to help & <br/>satisficed the local & global clients by web Development
-//                         <br/> solutions</p>           
-//                     </div>
-//                     <div className="button">
-//                     <Button variant="outline-primary" size='sm' className='w-25 rounded' >Contect</Button>{' '}
-//                     </div>
-//                     <div class="social-buttons">
-//                         <a href="https://www.instagram.com" class="social-button social-button--instagram" aria-label="Instagram">
-//                             <i class="bi bi-instagram"></i>
-//                         </a>
-//                         <a href="https://www.linkedin.com/home" class="social-button social-button--linkedin" aria-label="LinkedIn">
-//                             <i class="bi bi-linkedin"></i>
-//                         </a>
-//                         <a href="https://github.com/signup?user_email=&source=form-home-signup" class="social-button social-button--instagram" aria-label="Instagram">
-//                             <i class="bi bi-github"></i>
-//                         </a>
-//                         <a href="https://x.com/?lang=en" class="social-button social-button--linkedin" aria-label="LinkedIn">
-//                             <i class="bi bi-twitter"></i>
-//                         </a>
-//                         <a href="https://www.facebook.com/login/device-based/regular/login?_rdc=1&_rdr" class="social-button social-button--instagram" aria-label="Instagram">
-//                         <i class="bi bi-facebook"></i>
-//                         </a>
-//                     </div>
-//                 </div>
-//                 <div className="col-6">
-//                 <div class="vision">
-//                     <div class="image">
-//                         <img src="jayy.jpg" alt="" className='img-fluid'/>
-//                     </div> 
-//                 </div>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-//   )
-// }
 
-// export default Poster
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "../Poster.css"; // Import custom CSS file if needed
+import "../Poster.css"; 
 
 function Poster() {
   return (
